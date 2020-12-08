b19e7d33f895eaa94038b72e900bd4fa2273df83
Christian Juth <cjuth2@gmail.com>
2020-12-08 11:19:06 -0500

    hw7 sort of working

--- hw6-autograder/hw6/first.c
+++ hw6-autograder/hw6/first.c
@@ -4,70 +4,125 @@
 #include <unistd.h>
 #include <math.h>
 
+int reads;
+int writes;
+int hits;
+int misses;
+long int globalIndex;
+
 struct Node {
   int valid;
-  int tag;
+  long int tag;
+  long int index;
 };
-
-void hextobin(char hex[]) {  
-  int i=0;   
-  while(hex[i])  {  
-    switch(hex[i]) {  
-      case '0':  
-        printf("0000");  
-        break;  
-      case '1':  
-        printf("0001");  
-        break;  
-      case '2':  
-        printf("0010");  
-        break;  
-      case '3':  
-        printf("0011");  
-        break;  
-      case '4':  
-        printf("0100");  
-        break;  
-      case '5':  
-        printf("0101");  
-        break;  
-      case '6':  
-        printf("0110");  
-        break;  
-      case '7':  
-        printf("0111");  
-        break;  
-      case '8':  
-        printf("1000");  
-        break;  
-      case '9':  
-        printf("1001");  
-        break;  
-      case 'a':  
-        printf("1010");  
-        break;  
-      case 'b':  
-        printf("1011");  
-        break;  
-      case 'c':  
-        printf("1100");  
-        break;  
-      case 'd':  
-        printf("1101");  
-        break;  
-      case 'e':  
-        printf("1110");  
-        break;  
-      case 'f':  
-        printf("1111");  
-        break;  
-    }  
-    i++;  
-  }
-}  
 
 int cmpprefix(const char *pre, const char *str) {
   return strncmp(pre, str, strlen(pre));
+}
+
+long int getGlobalIndex() {
+  return globalIndex++;
+}
+
+void writeDirect(struct Node *cache, int setBits, long tagBits) {
+  // printf("%d == %d\n", cache[setBits].tag, tagBits);
+  writes++;
+
+  if (cache[setBits].valid == 1 && cache[setBits].tag == tagBits) {
+    // exsists in cache already
+    hits++;
+  } 
+
+  else {
+    // does not exsist in cache
+    reads++;
+    misses++;
+    cache[setBits].valid = 1;
+    cache[setBits].tag = tagBits;
+  }
+}
+
+void readDirect(struct Node *cache, int setBits, long tagBits) {
+  if (cache[setBits].valid == 1 && cache[setBits].tag == tagBits) {
+    // exsists in cache already
+    hits++;
+  } 
+
+  else {
+    // does not exsist in cache
+    misses++;
+    reads++;
+    cache[setBits].valid = 1;
+    cache[setBits].tag = tagBits;
+  }
+}
+
+void nAssocWrite(struct Node *cache, int setBits, long tagBits, int assoc, char policy) {
+  writes++;
+
+  int topOfSet = setBits * assoc;
+  int bottomOfSet = (setBits + 1) * assoc;
+
+  long int oldest = -1;
+  for (int r = topOfSet; r < bottomOfSet; r++) {
+    if (oldest == -1 || cache[r].index < oldest) {
+      oldest = cache[r].index;
+    }
+
+    if (cache[r].tag == tagBits) {
+      // exsists in cache already
+      if (policy == 'l') {
+        cache[r].index = getGlobalIndex();
+      }
+      hits++;
+      return;
+    }
+  }
+
+  // does not exsist in cache
+  reads++;
+  misses++;
+
+  for (int r = topOfSet; r < bottomOfSet; r++) {
+    if (cache[r].index == oldest) {
+      cache[r].index = getGlobalIndex();
+      cache[r].tag = tagBits;
+      break;
+    }
+  }
+}
+
+void nAssocRead(struct Node *cache, int setBits, long tagBits, int assoc, char policy) {
+  int topOfSet = setBits * assoc;
+  int bottomOfSet = (setBits + 1) * assoc;
+
+  long int oldest = -1;
+  for (int r = topOfSet; r < bottomOfSet; r++) {
+    if (oldest == -1 || cache[r].index < oldest) {
+      oldest = cache[r].index;
+    }
+
+    if (cache[r].tag == tagBits) {
+      // exsists in cache already
+      if (policy == 'l') {
+        cache[r].index = getGlobalIndex();
+      }
+      hits++;
+      return;
+    }
+  }
+
+  // does not exsist in cache
+  misses++;
+  reads++;
+
+  for (int r = topOfSet; r < bottomOfSet; r++) {
+    if (cache[r].index == oldest) {
+      cache[r].index = getGlobalIndex();
+      cache[r].tag = tagBits;
+      break;
+    }
+  }
 }
 
 int main(int argc, char* argv[])
@@ -75,30 +130,35 @@ int main(int argc, char* argv[])
 
   // is the total size of the cache in bytes. 
   // This number should be a power of 2
-  int cacheSize = atoi(argv[1]);
-  int assoc;
-  int setNum;
+  globalIndex = 1;
+  long cacheSize = atoi(argv[1]);
+  long assoc;
+  long setNum;
+  long cacheRows;
 
-  int blockSize = atoi(argv[4]);
+  long blockSize = atoi(argv[4]);
 
   // | Tag | Set | Offset |
-  int offsetNum = log2(blockSize);
+  long offsetNum = log2(blockSize);
 
   //  simulate a direct mapped cache
   if (strcmp(argv[2], "direct") == 0) {
     assoc = 1;
+    cacheRows = cacheSize / blockSize;
     setNum = log2(cacheSize / blockSize);
   } 
 
   // simulate a fully associative cache
   else if (strcmp(argv[2], "assoc") == 0) {
     assoc = cacheSize / blockSize;
-    setNum = log2(1);
+    cacheRows = cacheSize / blockSize;
+    setNum = log2(cacheSize / blockSize / assoc);
   } 
   
   // simulate an n-way associative cache. n will be a power of 2
   else if(cmpprefix("assoc:", argv[2]) == 0) {
-    sscanf(argv[2], "assoc:%d", &assoc);
+    sscanf(argv[2], "assoc:%ld", &assoc);
+    cacheRows = cacheSize / blockSize;
     setNum = log2(cacheSize / blockSize / assoc);
   }
 
@@ -131,29 +191,52 @@ int main(int argc, char* argv[])
   }
 
   char work;
-  unsigned long int address;
+  long int address;
 
   FILE * file = fopen(traceFile, "r");
 
-  int i = 0;
+  // direct map cache 
+  struct Node cache[cacheRows];
+  
+  for (int r = 0; r < cacheRows; r++) {
+    struct Node row;
+
+    row.index = -1;
+    row.tag = -1;
+    row.valid = 0;
+
+    cache[r] = row;
+  }
+
   while (fscanf(file, "%*x: %c %lx", &work, &address) == 2) {
+    // int offsetBits = address & ((1 << offsetNum) - 1);
+    long int setBits = (address >> offsetNum) & ((1 << setNum) - 1);
+    long int tagBits = (address >> (offsetNum + setNum));
 
-    int offsetBits = address & ((1 << offsetNum) - 1);
-    int setBits = (address >> offsetNum) & ((1 << setNum) - 1);
-    int tagBits = (address >> (offsetNum + setNum));
-
-    // printf("address: %ld\n", address);
-    // printf("tagBits, setBits: %d, offsetBits: %d\n", setNum, offsetNum);
-    printf("tag: %d, set: %d\n", tagBits, setBits);
-    
-    if (i == 20) {
-      break;
+    // direct
+    if (strcmp(argv[2], "direct") == 0) {
+      if (work == 'R') {
+        readDirect(cache, setBits, tagBits);
+      } 
+      
+      else if (work == 'W') {
+        writeDirect(cache, setBits, tagBits);
+      }
     }
-    i++;
+
+    // associative
+    else {
+      if (work == 'R') {
+        nAssocRead(cache, setBits, tagBits, assoc, policy);
+      } 
+      
+      else if (work == 'W') {
+        nAssocWrite(cache, setBits, tagBits, assoc, policy);
+      }
+    }
   }
 
   fclose(file);
 
-  // hextobin("804ae19");
-
+  printf("Memory reads: %d\nMemory writes: %d\nCache hits: %d\nCache misses: %d\n", reads, writes, hits, misses);
 }
---
+++ hw7-autograder/hw7/first.c
@@ -0,0 +1,239 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// struct Arg {
+//   char name[20];
+//   struct Arg* next;
+// }
+
+struct Instruction {
+  char args[4][20];
+  struct Instruction* next;
+};
+
+struct TmpVar {
+  int value;
+  char name[20];
+  struct TmpVar* next;
+};
+
+int getTmpVar(struct TmpVar *crnt, char *name) {
+  while (crnt != NULL) {
+    if (strcmp(crnt->name, name) == 0) {
+      return crnt->value;
+    }
+    crnt = crnt->next;
+  }
+  return -1;
+}
+
+void setTmpVar(struct TmpVar *crnt, char *name, int value) {
+  struct TmpVar *prev = NULL;
+  while (crnt != NULL) {
+    if (strcmp(crnt->name, name) == 0) {
+      crnt->value = value;
+      return;
+    }
+    prev = crnt;
+    crnt = crnt->next;
+  }
+
+  struct TmpVar* new = (struct TmpVar*)malloc(sizeof(struct TmpVar));
+  strcpy(new->name, name);
+  new->value = value;
+  prev->next = new;
+}
+
+
+void readParamNames(char *input, char output[][20]) {
+  char crnt[20] = "";
+
+  int varIndex = 0;
+  int crntLength = 0;
+  for (int i = 0; i < strlen(input); i++) {
+    if (input[i] == '\t' || input[i] == ' ' || input[i] == '\n') {
+      strcpy(output[varIndex], crnt);
+      memset(crnt, 0, strlen(crnt));
+      varIndex++;
+      crntLength = 0;
+      continue;
+    }
+
+    crnt[crntLength] = input[i];
+    crntLength++;
+  }
+  
+  if (crntLength > 0) {
+    strcpy(output[varIndex], crnt);
+  }
+}
+
+void setInputValues(int *ref, int numOfValues, int val) {
+  int crnt = val;
+  for (int i = 0; i < numOfValues; i++) {
+    int bit = crnt & 1;
+    crnt = crnt >> 1;
+    ref[numOfValues - 1 - i] = bit;
+  }
+}
+
+int getInputVarValue(int *refValues, char refNames[][20], int numOfValues, char *name) {
+  for (int i = 0; i < numOfValues; i++) {
+    if (strcmp(refNames[i], name) == 0) {
+      return refValues[i];
+    }
+  }
+  return -1;
+}
+
+int setOutputVarValue(int *refValues, char refNames[][20], int numOfValues, char *name, int value) {
+  for (int i = 0; i < numOfValues; i++) {
+    if (strcmp(refNames[i], name) == 0) {
+      refValues[i] = value;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int main(int argc, char* argv[])
+{
+  char *fileName = argv[1];
+  FILE* file = fopen(fileName, "r"); 
+  char output[50] = "";
+  char tmp[50] = "";
+
+  if(access(fileName, F_OK) == -1) {
+    printf("error");
+    return 0;
+  }
+
+  int inputLength = 0;
+  int outputLength = 0;
+
+  fgets(output, 50, file);
+  sscanf(output, "INPUTVAR %d %[^\n]s", &inputLength, tmp);
+  char inputNames[inputLength][20];
+  readParamNames(tmp, inputNames);
+
+  fgets(output, 50, file);
+  sscanf(output, "OUTPUTVAR %d %[^\n]s", &outputLength, tmp);
+  char outputNames[outputLength][20];
+  readParamNames(tmp, outputNames);
+
+  struct Instruction* front = NULL;
+  struct Instruction* prev = NULL;
+
+  while (fgets(output, 50, file)) {
+    struct Instruction* crnt = (struct Instruction*)malloc(sizeof(struct Instruction));
+    // crnt->args[0] = malloc(80);
+    readParamNames(output, crnt->args);
+
+    if (front == NULL) {
+      front = crnt;
+    } else {
+      prev->next = crnt;
+      prev = crnt;
+    }
+  }
+
+  int max = 1 << inputLength;
+  int inputValues[inputLength];
+  int outputValues[inputLength];
+
+  struct Instruction* crnt;
+  struct TmpVar* tmpVarFront = (struct TmpVar*)malloc(sizeof(struct TmpVar));
+
+  for (int i = 0; i < max; i++) {
+    crnt = front;
+    setInputValues(inputValues, inputLength, i);
+
+    while (crnt != NULL) {
+      int computedValue = 0;
+
+      char command[20] = "";
+      strcpy(command, crnt->args[0]);
+
+      int arg1 = getInputVarValue(inputValues, inputNames, inputLength, crnt->args[1]);
+      if (arg1 == -1) {
+        arg1 = getTmpVar(tmpVarFront, crnt->args[1]);
+      }
+
+      char outputName[20] = "";
+
+      // HANDLE NOT
+      if (strcmp(command, "NOT") == 0) {
+        strcpy(outputName, crnt->args[2]);
+        computedValue = ~arg1;
+      } 
+      
+      else {
+        strcpy(outputName, crnt->args[3]);
+
+        int arg2 = getInputVarValue(inputValues, inputNames, inputLength, crnt->args[2]);
+        if (arg2 == -1) {
+          arg2 = getTmpVar(tmpVarFront, crnt->args[2]);
+        }
+
+        if (strcmp(command, "AND") == 0) {
+          computedValue = arg1 & arg2;
+        }
+
+        else if (strcmp(command, "OR") == 0) {
+          computedValue = arg1 | arg2;
+        }
+
+        else if (strcmp(command, "NAND") == 0) {
+          computedValue = (arg1 & arg2) != 1;
+        }
+
+        else if (strcmp(command, "NOR") == 0) {
+          computedValue = (arg1 | arg2) != 1;
+        }
+
+        else if (strcmp(command, "NOR") == 0) {
+          if ((arg1 & arg2) == 1) {
+            computedValue = 0;
+          } else {
+            computedValue = arg1 | arg2;
+          }
+        }
+      }
+
+      if (setOutputVarValue(outputValues, outputNames, outputLength, outputName, computedValue) == -1) {
+        setTmpVar(tmpVarFront, outputName, computedValue);
+      }
+      crnt = crnt->next;
+    }
+
+    // print array
+    for (int j = 0; j < inputLength; j++) {
+      printf("%d ", inputValues[j]);
+    }
+    for (int j = 0; j < outputLength; j++) {
+      printf("%d ", outputValues[j]);
+    }
+    printf("\n");
+  }
+
+
+  // Cleanup
+  // for (int i = 0; i < inputLength; i++) {
+  //   free(inputNames[i]);
+  // }
+  // for (int i = 0; i < outputLength; i++) {
+  //   free(outputNames[i]);
+  // }
+  while (tmpVarFront != NULL) {
+    struct TmpVar* next = tmpVarFront->next;
+    free(tmpVarFront);
+    tmpVarFront = next;
+  }
+  while (front != NULL) {
+    struct Instruction* next = front->next;
+    free(front);
+    front = next;
+  }
+}
